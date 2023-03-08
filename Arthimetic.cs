df35c14c831e925f84b9465082f42ba956e6c2a1
ADMIN$ <ADMIN$@DESKTOP-9TIC5VQ>
2023-03-08 12:31:32 +0530

    Simple And Complex Function , Parameter Type

---
+++ Arthimetic.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+	internal class Arthimetic
+	{
+		// Simple Function Demo
+		#region Simple Function
+		public static void SimpleFunction(int num1,int num2,int num3,int num4,int num5) // Parameter OR Arguments
+		{
+			int result = num1 + num2 - num3 * num4 / num5;
+			Console.WriteLine($"{num1} + {num2} - {num3} * {num4} / {num5} = {result}");
+		}
+		#endregion
+
+		// Complex Function Demo
+		#region Complex Function
+
+		public int A;public int B; public int C; public int D;
+		public void ComplexFunction(Arthimetic ArthiMetic) // Object Of a Class
+		{
+			int result1 = ArthiMetic.A + ArthiMetic.C - ArthiMetic.D * ArthiMetic.B;
+			Console.WriteLine($"{ArthiMetic.A} + {ArthiMetic.C} - {ArthiMetic.D} * {ArthiMetic.D} = {result1}");
+		}
+		#endregion
+
+		// Types Of Parameter
+
+		#region 1.Input Parameter
+		public static void AdditionI(int n1,int n2)
+		{
+			int result3 = n1 + n2;
+
+			Console.WriteLine($"{n1} + {n2} = {result3}");
+		}
+
+		public static void SubstractionI(int n3,int n4)
+		{
+			int result3 = n3 - n4;
+
+			Console.WriteLine($"{n3} - {n4} = {result3}");
+		}
+
+		public static void DivisonI(int n5,int n6)
+		{
+			int result3 = n5 / n6;
+
+			Console.WriteLine($"{n5} / {n6} = {result3}");
+		}
+		#endregion
+
+		#region 2.Output Parameter
+
+		// Addition
+		public static void AdditionO(int nm1,int nm2,out int result4) // Output Parameter
+		{
+			result4 = nm1 + nm2;
+			Console.WriteLine($"Addition Of {nm1} And {nm2} Is = {result4}");
+		}
+
+		//Substraction
+		public static void SubstractionO(int nb1,int nb2,out int result5)
+		{
+			result5= nb1 - nb2;
+			Console.WriteLine($"Substraction Of {nb1} And {nb2} Is = {result5}");
+		}
+
+		//Multiplication
+		public static void MultiplicationO(int nub1, int nub2, out int result6)
+		{
+			result6 = nub1 * nub2;
+			Console.WriteLine($"Multiplication Of {nub1} And {nub2} Is = {result6}");
+		}
+		#endregion
+
+		#region 3. Input/Output Parameter
+
+		public void Add(int w,int x,ref int result6)
+		{
+			result6 = result6 + w + x;
+			Console.WriteLine($"{w} + {x} = {result6}");
+		}
+
+		public void Sub(int r, int s, ref int result7)
+		{
+			result7 = result7 - r - s;
+			Console.WriteLine($"{r} - {s} = {result7}");
+		}
+
+		public void Mul(int g, int h, ref int result8)
+		{
+			result8 = result8 * g * h;
+			Console.WriteLine($"{g} * {h} = {result8}");
+		}
+		#endregion
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,72 @@ public class Program
 {
 	public static void Main()
 	{
+		// Simple Function
+		//Arthimetic.SimpleFunction(10, 20, 20, 10, 5);
+
+
+
+		// Complex Function
+		//Arthimetic arthimetic1 = new Arthimetic();
+		//arthimetic1.A = 10; arthimetic1.B = 30; arthimetic1.C = 50; arthimetic1.D = 10;
+
+		//Arthimetic arthimetic2 = new Arthimetic();
+		//arthimetic2.ComplexFunction(arthimetic1);
+
+		// Input Parameter
+		//Arthimetic.AdditionI(11,22);        // Addition
+		//Arthimetic.SubstractionI(34,12);    // Substraction
+		//Arthimetic.DivisonI(10,20);         // Multiplication
+
+
+		// Output Parameter
+		//int result4;                                  // Additon 
+		//Arthimetic.AdditionO(10,30,out result4);
+
+		//int result5;                                  // Substraction
+		//Arthimetic.SubstractionO(20,70,out result5);
+
+		//int result6;                                  // Multiplication
+		//Arthimetic.MultiplicationO(10,30,out result6);
+
+
+		// Input/Output Parameter
+		Arthimetic arthimetic3= new Arthimetic();
+
+		int result6 = 2;                     // Addition
+		arthimetic3.Add(2,3,ref result6);
+		Console.WriteLine($"Addition Result = {result6}");
+
+		int result7 = 4;                     // Substraction
+		arthimetic3.Sub(4,5,ref result7);
+		Console.WriteLine($"Substraction Result = {result7}");
+
+		int result8 = 1;                     // Multiplication
+		arthimetic3.Mul(10,4,ref result8);
+		Console.WriteLine($"Multiplication Result = {result8}");
+
+
+
+
+
+
+
+
+
 
 		// Non- Returning Function And Returning Function
-		
-		Doctor doctor1 = new Doctor();
-		doctor1.GetData();
-		doctor1.PutData();
+
+		//Console.WriteLine("Object1");
+		//Doctor doctor1 = new Doctor();
+		//doctor1.GetData();
+		//doctor1.PutData();
+
+		//Console.WriteLine();
+
+		//Console.WriteLine("Object2");
+		//Doctor doctor2 = new Doctor();
+		//doctor2.GetData();
+		//doctor2.PutData();
 
 		// Typecasting
 		//Doctor.TypeCasting();
